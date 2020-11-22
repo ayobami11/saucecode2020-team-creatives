eedf8f3dd87181c6b3b8bd797e40a1ebd321d3bc
ShowBaba <samwise858@gmail.com>
2020-11-22 21:40:30 +0100

    fix syntaxerror: unexpected token i in json at position 0

--- js/formValidation.js
+++ js/formValidation.js
@@ -21,18 +21,23 @@ const validateFormInputs = async (event) => {
         const form = event.target;
         const url = form.action;
         const formData = new FormData(form);
-
+        let user = {}
+        formData.forEach((value, key) => {
+            user[key] = value;
+        })
+        console.log(user)
         try {
             const response = await fetch(url, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json; charset=UTF-8'
                 },
-                body: JSON.stringify(formData)
+                body: JSON.stringify(user)
             });
 
             console.log(response);
             const jsonResponse = await response.json();
+            console.log(jsonResponse)
             if (jsonResponse.success) {
                 console.log('Successful');
             } else {
